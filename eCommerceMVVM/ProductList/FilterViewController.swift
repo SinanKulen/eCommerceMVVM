53615b16621a3d3d81fb6bdb356aed8b462e8f8a
Sinan Kulen <sinan.kulen@gmail.com>
2022-03-23 23:28:00 +0300

    adjusted FilterViewController

--- eCommerceMVVM/ProductList/FilterViewController.swift
+++ eCommerceMVVM/ProductList/FilterViewController.swift
@@ -10,49 +10,49 @@ import SnapKit
 
 class FilterViewController: UIViewController {
     
-    lazy var  menuButton : UIButton = {
-        var view = UIButton()
-        view.setTitle("Electronics", for:.normal)
-        view.addTarget(nil, action: #selector(tappedMenuButton(sender:)), for: .touchUpInside)
-        view.frame = CGRect(x: 0, y: 0, width: 50, height: 30)
-        view.backgroundColor = .red
-        
-        var config = UIButton.Configuration.gray()
-        config.cornerStyle = .capsule
-        view.configuration = config
-        
-        view.isUserInteractionEnabled = true
-        return view
-    }()
-    
-    lazy var filterBackground : UIView = {
-        var view = UIView()
-        view.frame = CGRect(x: 0, y: 0, width: 200, height: 180)
-        view.backgroundColor = .blue
-        return view
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setup()
-    }
-    
-    func setup() {
-        view.backgroundColor = .white
-        view.addSubview(filterBackground)
-        filterBackground.snp.makeConstraints { make in
-            make.right.left.top.bottom.equalToSuperview().offset(10)
-        }
-      
-        filterBackground.addSubview(menuButton)
-        menuButton.snp.makeConstraints { make in
-            make.top.equalTo(filterBackground).offset(100)
-            make.left.equalTo(filterBackground).offset(50)
-        }
-    }
-    
-    @objc func tappedMenuButton(sender: UIButton) {
-        print("tıklandı")
-    }
-    
+//    lazy var  menuButton : UIButton = {
+//        var view = UIButton()
+//        view.setTitle("Electronics", for:.normal)
+//        view.addTarget(nil, action: #selector(tappedMenuButton(sender:)), for: .touchUpInside)
+//        view.frame = CGRect(x: 0, y: 0, width: 50, height: 30)
+//        view.backgroundColor = .red
+//
+//        var config = UIButton.Configuration.gray()
+//        config.cornerStyle = .capsule
+//        view.configuration = config
+//
+//        view.isUserInteractionEnabled = true
+//        return view
+//    }()
+//
+//    lazy var filterBackground : UIView = {
+//        var view = UIView()
+//        view.frame = CGRect(x: 0, y: 0, width: 200, height: 180)
+//        view.backgroundColor = .blue
+//        return view
+//    }()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        setup()
+//    }
+//
+//    func setup() {
+//        view.backgroundColor = .white
+//        view.addSubview(filterBackground)
+//        filterBackground.snp.makeConstraints { make in
+//            make.right.left.top.bottom.equalToSuperview().offset(10)
+//        }
+//
+//        filterBackground.addSubview(menuButton)
+//        menuButton.snp.makeConstraints { make in
+//            make.top.equalTo(filterBackground).offset(100)
+//            make.left.equalTo(filterBackground).offset(50)
+//        }
+//    }
+//
+//    @objc func tappedMenuButton(sender: UIButton) {
+//        print("tıklandı")
+//    }
+//
 }
